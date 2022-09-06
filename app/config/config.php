5a73978d173e3ae0fd661dee11cc5fc6b1e49131
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-06 10:43:28 +0300

    Initial project setup

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','');
+//APP ROOT
+define('APPROOT',dirname(dirname(__FILE__)));
+//url root
+define('URLROOT','http://localhost/mvcframework');
+//SITE NAME
+define('SITENAME','MACKMVC');
