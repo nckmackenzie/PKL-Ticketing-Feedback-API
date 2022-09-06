857c392952ed93498ecf4999810f8fd3f28b4280
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-06 10:46:17 +0300

    Add DB_NAME value and Change URLROOT value

--- app/config/config.php
+++ app/config/config.php
@@ -3,10 +3,8 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','');
+define('DB_NAME','pkl_ticketing_feedback');
 //APP ROOT
 define('APPROOT',dirname(dirname(__FILE__)));
 //url root
-define('URLROOT','http://localhost/mvcframework');
-//SITE NAME
-define('SITENAME','MACKMVC');
+define('URLROOT','http://localhost/pkl_api');
