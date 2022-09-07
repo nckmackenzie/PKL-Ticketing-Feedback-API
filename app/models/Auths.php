848a8c4691341dfb0d7dced20b61828859b73d8c
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-09-07 15:44:11 +0300

    Add register method

--- app/models/Auths.php
+++ app/models/Auths.php
@@ -11,4 +11,20 @@ class Auths
     {
         return checkexists($this->db->dbh,'users','email',$id,$val);
     }
+
+    public function register($data)
+    {
+        $this->db->query('INSERT INTO users (`user_name`,`password`,`email`,`contact`,`user_type_id`) 
+                          VALUES(:uname,:pwd,:email,:contact,:usid)');
+        $this->db->bind(':uname',!empty($data->user_name) ? trim(strtolower($data->user_name)) : NULL);
+        $this->db->bind(':pwd',!empty($data->password) ? password_hash(trim(strtolower($data->user_name)),PASSWORD_DEFAULT) : NULL);
+        $this->db->bind(':email',!empty($data->email) ? trim(strtolower($data->email)) : NULL);
+        $this->db->bind(':contact',!empty($data->contact) ? trim(strtolower($data->contact)) : NULL);
+        $this->db->bind(':usid',!empty($data->user_type) ? trim(strtolower($data->user_type)) : NULL);
+        if(!$this->db->execute()) :
+            return false;
+        endif;
+        $id = $this->db->getlastinsertid();
+        return returninsertedrow($this->db->dbh,'users',$id);  
+    }
 }
