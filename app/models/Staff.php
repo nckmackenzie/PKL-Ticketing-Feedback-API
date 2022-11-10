a71d0d9c6832001d4c6a35d8b910db32e4cf1c9b
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-11-10 11:02:27 +0300

    constructor setup and method to create staff

---
+++ app/models/Staff.php
@@ -0,0 +1,31 @@
+<?php
+
+class Staff
+{
+    private $db;
+    
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function Create($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+
+            $this->db->query('INSERT INTO staffs (StaffName,StaffDept) VALUES(:sname,:sdept)');
+            $this->db->bind(':sname',$data['staffname']);
+            $this->db->bind(':sdept',$data['staffdept']);
+            $this->db->execute();
+            $tid = $this->db->dbh->lastInsertId();
+            if(!$this->db->dbh->commit()) return false;
+
+            return returninsertedrow($this->db->dbh,'staffs',$tid); 
+            
+        } catch (PDOException $th) {
+            error_log($th->getMessage(),0);
+            return false;
+        }
+    }
+}
