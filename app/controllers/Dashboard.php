73e4c7d928366bb53215844cc957b58efc5bac35
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-11-11 14:13:01 +0300

    functionality to get dashboard data for charts

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -12,9 +12,32 @@ class Dashboard extends Controller
     {
         if($_SERVER['REQUEST_METHOD'] === 'GET')
         {
+            $data = [];
+            //summary information for cards
             $results = $this->dashboardmodel->GetSummaryInfo();
             $summary = ['open' => $results[0],'closed' => $results[1],'pending' => $results[2]];
-            sendresponse(200,null,true,$summary);
+            $data['summarycard'] = $summary;
+            //chart data for by priority chart
+            $prioritydata = [];
+            foreach($this->dashboardmodel->ByPriority() as $result)
+            {
+                array_push($prioritydata,[
+                    'priority' => ucwords($result->Priority),
+                    'value' => $result->RecordCount,
+                ]);
+            }
+            $data['bypriority'] = $prioritydata;
+            //chart data for by dept
+            $departmentdata = [];
+            foreach($this->dashboardmodel->ByDepartment() as $result)
+            {
+                array_push($departmentdata,[
+                    'department' => ucwords($result->StaffDept),
+                    'value' => $result->RecordCount,
+                ]);
+            }
+            $data['bydepartment'] = $departmentdata;
+            sendresponse(200,null,true,$data);
         }
         elseif ($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
         {
--- app/models/Dashboards.php
+++ app/models/Dashboards.php
@@ -17,4 +17,22 @@ class Dashboards
         $pendingtickets = getdbvalue($this->db->dbh,$sql,['pending']);
         return [$opentickets,$closedtickets,$pendingtickets];
     }
+
+    public function ByPriority()
+    {
+        return loadresultset($this->db->dbh,'SELECT DISTINCT Priority,fn_get_priority_count(Priority) As RecordCount
+                                             FROM `cases` WHERE (Deleted = 0) AND (`Status` <> ?) ',['closed']);
+    }
+
+    public function ByStatus()
+    {
+        return loadresultset($this->db->dbh,'SELECT DISTINCT Status,fn_get_status_count(`Status`) As RecordCount 
+                                             FROM `cases` WHERE (Deleted = 0) AND (`Status` <> ?)',['closed']);
+    }
+
+    public function ByDepartment()
+    {
+        return loadresultset($this->db->dbh,'SELECT DISTINCT `StaffDept`,fn_get_department_count(StaffDept) As RecordCount 
+                                             FROM `vw_cases_by_dept`',[]);
+    }
 }
