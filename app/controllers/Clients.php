101468e01883137f7943dc4913a0fab59d190c9d
nicmackenzie <nicmackenzie@live.com>
2023-04-06 13:57:15 +0300

    Add customer functionality

--- app/controllers/Clients.php
+++ app/controllers/Clients.php
@@ -13,6 +13,35 @@ class Clients extends Controller
     {
         if($_SERVER['REQUEST_METHOD'] === 'POST')
         {
+            $messages = [];
+            $fields = json_decode(file_get_contents('php://input'));
+            $data = [
+                'customername' => isset($fields->customerName) && !empty(trim($fields->customerName)) ? trim($fields->customerName) : null,
+                'contact' => isset($fields->contact) && !empty(trim($fields->contact)) ? '254'. substr(trim($fields->contact),1) : null,
+            ];
+
+            
+            //validate
+            if(is_null($data['customername'])){
+                array_push($messages,'Provide client name');
+            }
+            if(is_null($data['contact'])){
+                array_push($messages,'Provide client contact');
+            }
+
+            if(count($messages) > 0){
+                sendresponse(400,$messages,false);
+                exit();
+            }
+
+            $newclient = $this->clientmodel->Create($data);
+            if(!$newclient){
+                sendresponse(500,'Something went wrong when creating staff',false);
+                exit;
+            }
+            $client =['customerName' => ucwords($newclient->ClientName),'id' => $newclient->ID];
+            sendresponse(200,null,true,$client);
+            exit;
 
         }
         elseif($_SERVER['REQUEST_METHOD'] === 'GET')
@@ -23,7 +52,7 @@ class Clients extends Controller
             {
                 array_push($data,[
                     'id' => $customer->ID,
-                    'customerName' => $customer->ClientName
+                    'customerName' => ucwords($customer->ClientName)
                 ]);
             }
             sendresponse(200,null,true,$data);
--- app/models/Client.php
+++ app/models/Client.php
@@ -13,4 +13,24 @@ class Client
         $sql = 'SELECT ID,UCASE(ClientName) AS ClientName FROM clients ORDER BY ClientName';
         return loadresultset($this->db->dbh,$sql,[]);
     }
+
+    public function Create($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+
+            $this->db->query('INSERT INTO clients (ClientName,Contact) VALUES(:cname,:contact)');
+            $this->db->bind(':cname',$data['customername']);
+            $this->db->bind(':contact',$data['contact']);
+            $this->db->execute();
+            $tid = $this->db->dbh->lastInsertId();
+            if(!$this->db->dbh->commit()) return false;
+
+            return returninsertedrow($this->db->dbh,'clients',$tid); 
+            
+        } catch (PDOException $th) {
+            error_log($th->getMessage(),0);
+            return false;
+        }
+    }
 }
