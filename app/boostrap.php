14f17e30afc9301980c038dcc19c3fc55b1474bf
nicmackenzie <nicmackenzie@live.com>
2023-04-06 11:59:11 +0300

    change order of requires

--- app/boostrap.php
+++ app/boostrap.php
@@ -1,8 +1,8 @@
 <?php
 //load config 
+require_once 'helpers/setheaders.php';
 require_once 'config/config.php';
 require_once 'helpers/JwtAuth.php';
-require_once 'helpers/setheaders.php';
 require_once 'helpers/utils.php';
 //load Libraries
 // require_once 'libraries/Core.php';
